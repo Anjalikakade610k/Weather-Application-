88b00419eb2f232b91cfec4bc6db91723572076a
Anjalikakade610k <71441240+Anjalikakade610k@users.noreply.github.com>
2023-01-26 20:14:03 -0500

    Weather-App

---
+++ Weather-App(React)/precache-manifest.3b80db95d5699ca3f8d0a734ce97973d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7abbec1191fe03f02e43a655328971bd",
+    "url": "/react-weatherApp/index.html"
+  },
+  {
+    "revision": "18983a72b7720e370526",
+    "url": "/react-weatherApp/static/css/2.6605e475.chunk.css"
+  },
+  {
+    "revision": "054aa8af27f61a998368",
+    "url": "/react-weatherApp/static/css/main.6a9a0fef.chunk.css"
+  },
+  {
+    "revision": "18983a72b7720e370526",
+    "url": "/react-weatherApp/static/js/2.ee441b32.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-weatherApp/static/js/2.ee441b32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "054aa8af27f61a998368",
+    "url": "/react-weatherApp/static/js/main.5d5e1366.chunk.js"
+  },
+  {
+    "revision": "54ccf4bd76470c6c80d6",
+    "url": "/react-weatherApp/static/js/runtime-main.7085200e.js"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/react-weatherApp/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/react-weatherApp/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/react-weatherApp/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/react-weatherApp/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/react-weatherApp/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
